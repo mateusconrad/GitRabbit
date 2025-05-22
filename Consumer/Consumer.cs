ead452edf98a696da7730dd4c85ad4107c8bfc35
Mateus Conrad <mateus.conrad.a@gmail.com>
2025-05-21 23:31:32 -0300

    First Commit - Consumer and Sender

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Abre o canal de comunicação com a fila esperada
+channel.QueueDeclare(
+    queue: "Basic Test", 
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+// Cria o consumidor da mensagem
+var consumer = new EventingBasicConsumer(channel);
+
+// recebe a mensagem
+consumer.Received+= (model, ea) =>
+{
+    var body = ea.Body;
+    var message = Encoding.UTF8.GetString(body.ToArray());
+    Console.WriteLine($"[x]Recebeu mensagem {message}");
+};
+
+// marca a mensagem como consumida
+channel.BasicConsume("Basic Test", true, consumer);
+
+
+Console.WriteLine("Press [enter] to exit ");
+
+Console.ReadLine();
---
+++ Sender/Sender.cs
@@ -0,0 +1,37 @@
+﻿// Sender/ Producer
+
+using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+// Declara a fila
+channel.QueueDeclare(
+    queue: "Basic Test", // Precisa ser igual ao routing key
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+string message = "Começando a entender esa bagaça";
+
+// declara o corpo da mensagem, transformando em bytes
+var body = Encoding.UTF8.GetBytes(message);
+
+// envia a mensagem
+channel.BasicPublish(
+    exchange: "", 
+    routingKey: "Basic Test", // Precisa ser igual ao nome da queue
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine($" [x] Sent : {message}");
+
+
+Console.WriteLine("Press [enter] to exit");
+
+// Console.ReadLine();
